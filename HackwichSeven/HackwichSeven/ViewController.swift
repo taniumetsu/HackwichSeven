4a7de0b01f7721eb0e04956ee38b1e404ee74998
Tani Umetsu <taniu@hawaii.edu>
2023-10-12 13:45:25 -1000

    initial commit

---
+++ HackwichSeven/HackwichSeven/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  HackwichSeven
+//
+//  Created by Tani Umetsu on 10/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        displayLabel.text = ""
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func setLabelButtonPressed(_ sender: Any)
+    {
+        
+    }
+    
+}
+
